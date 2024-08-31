fea2f4b21c21e24bb475c8d2fe68afec459b8341
NATHAN JOHN CORDEIRO <155100838+NathanCordeiro@users.noreply.github.com>
2024-08-31 14:42:31 +0530

    Add files via upload

---
+++ Exp2/P1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Programmer name: Nathan John Cordeiro
+#Program title: Display greet message using the system time.
+
+
+hour=`date +%H`
+if [ $hour -lt 12 ] # if hour is less than 12
+then
+echo "GOOD MORNING WORLD"
+elif [ $hour -le 16 ] # if hour is less than equal to 16
+then
+echo "GOOD AFTERNOON WORLD"
+elif [ $hour -le 20 ] # if hour is less than equal to 20
+then
+echo "GOOD EVENING WORLD"
+else
+echo "GOOD NIGHT WORLD"
+fi
